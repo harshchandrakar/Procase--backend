51b0f61f9a3f58be4d66e31ce63baaeb8d1480cf
harshchandrakar <harshc252@gmail.com>
2022-06-05 10:33:48 +0530

    new files

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,35 +1,41 @@
-const express = require("express")
-const {registerUser,userLogin,forgotPassWord,verifyOtp,updatePassword}  = require("../controller/authController")
-const multer = require('multer')
+const express = require("express");
+const {
+  registerUser,
+  userLogin,
+  forgotPassWord,
+  verifyOtp,
+  updatePassword,
+} = require("../controller/authController");
+const multer = require("multer");
 
 const storage = multer.diskStorage({
-    destination: function(req,file,cb){
-        cb(null,'uploads/');
-    },
-    filename: function(req,file,cb){
-        cb(null,new Date().toISOString().replace(/:/g, '-')+ file.originalname)
-    }
-})
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toISOString().replace(/:/g, "-") + file.originalname);
+  },
+});
 
-const fileFilter = (req,file,cb) =>{
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
-        cb(null,true)
-    }else{
-        cb(null,false)
-    }
-}
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
-const upload =multer({storage:storage , fileFilter:fileFilter})
-const router = express.Router()
+const upload = multer({ storage: storage, fileFilter: fileFilter });
+const router = express.Router();
 
-router.route("/user/register").post(upload.single('productImage'),registerUser)
+router.route("/user/register").post(upload.single("avatar"), registerUser);
 
-router.route("/user/login").post(userLogin)
+router.route("/user/login").post(userLogin);
 
-router.route("/user/forgot").post(forgotPassWord)
+router.route("/user/forgot").post(forgotPassWord);
 
-router.route("/user/otp/verify").post(verifyOtp)
+router.route("/user/otp/verify").post(verifyOtp);
 
-router.route("/user/updatePassword").patch(updatePassword)
+router.route("/user/updatePassword").patch(updatePassword);
 
-module.exports = router
+module.exports = router;
