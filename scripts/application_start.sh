1d8496c53c5426ed74cc8488cbb25c88a8ef0008
harshchandrakar <harshc252@gmail.com>
2022-06-18 23:00:27 +0530

    added /

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,7 +4,7 @@
 sudo chmod -R 777 /home/ec2-user/procase
 
 #navigate into our working directory where we have all our github files
-cd /home/ec2-user/procase
+cd /home/ec2-user/procase/
 
 #add npm and node to path
 export NVM_DIR="$HOME/.nvm"	
