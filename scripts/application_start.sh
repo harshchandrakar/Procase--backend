5a448805fb07937b553bd42d87d9d9821a29bd8f
harshchandrakar <harshc252@gmail.com>
2022-06-19 00:00:41 +0530

    removed /

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,13 @@
 	#!/bin/bash
 
+DIR="/home/ec2-user/procase"
+if [ -d "$DIR" ]; then
+  echo "${DIR} exists"
+else
+  echo "Creating ${DIR} directory"
+  mkdir ${DIR}
+fi
+
 #give permission for everything in the express-app directory
 sudo chmod -R 777 /home/ec2-user/procase
 
